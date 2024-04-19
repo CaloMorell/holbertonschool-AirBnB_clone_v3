fd07711d050648a3ddf5ccc7f0a0bedec3a76308
William <titanbabrisa@gmail.com>
2024-04-19 18:20:06 -0400

    fix pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,9 +14,11 @@ def close(exception):
 
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     api_host = getenv("HBNB_API_HOST", default='0.0.0.0')
