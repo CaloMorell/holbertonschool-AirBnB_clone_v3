271fa74804218c4f6e53227a71cbf3b44bf3a988
William <titanbabrisa@gmail.com>
2024-04-19 19:23:31 -0400

    added config to request calls so they use silent=True

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,9 +43,9 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Create a state"""
-    if not request.get_json():
+    if not request.get_json(silent=True):
         abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
+    if 'name' not in request.get_json(silent=True):
         abort(400, description="Missing name")
     state = State(**request.get_json())
     state.save()
@@ -59,7 +59,7 @@ def update_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.get_json():
+    if not request.get_json(silent=True):
         abort(400, description="Not a JSON")
     for key, value in request.get_json().items():
         if key not in ['id', 'created_at', 'updated_at']:
