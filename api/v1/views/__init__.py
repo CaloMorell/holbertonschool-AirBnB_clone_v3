763c30a962a882b7686b0c1ca436efff7d6917c2
CaloMorell <oscarmorell07@yahoo.com>
2024-04-19 18:50:44 -0400

    placesreviews

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+
 """This module is a python package that creates a blueprint from flask"""
+
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.cities import *
